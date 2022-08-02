5cba07fea4d088d36a3e1c0cc41d370ae2176420
MariielDesign <mariel.graphicdesigner@gmail.com>
2022-08-01 20:59:09 -0500

    Button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
+        <h1> ¡MARIEL TU PUEDES!</h1>
+        <button variant="primary">Aceptar</button>
       </header>
     </div>
   );
